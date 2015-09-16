e798f1e785d615daf1edb65dc40d89fe68079f15
teamfritz <cyberpatriotfritz@gmail.com>
2015-09-16 13:58:16 -0700

    9/16-1

--- workspace/Test Project/src/helloworld/helloworld.java
+++ workspace/Test Project/src/helloworld/helloworld.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 
 public class helloworld {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+		int input;
+		String input2;
 		System.out.println("Hello wolrd");
-		Scanner input = new Scanner(System.in);
+		Scanner read = new Scanner(System.in);
+		System.out.println("Please enter the current temperature");
+		input = read.nextInt();
+		System.out.println("Would you like to go from celcius to FARENHEIT or ftom farenheit to CELCIUS");
 	}
 
 }
---
+++ workspace/Test Project/src/helloworld/tempurature.java
@@ -0,0 +1,34 @@
+
+package helloworld;
+
+import java.util.Scanner;
+
+public class tempurature {
+
+	public static void main(String[] args) {
+		double input = 0, output = 0, type = 0;
+		boolean valid = false;
+		Scanner read = new Scanner(System.in);
+		while(valid == false) {
+			System.out.println("Would you like to go from celcius to farenheit [1] or ftom farenheit to celcius [2]");
+			type = read.nextDouble();
+			if (type==1||type==2) {
+				valid = true;
+			}
+			else {
+				System.out.println("Invalid Response. Must be within 1 to 2");
+			}
+		}
+		System.out.println("Please enter the current temperature");
+		input = read.nextDouble();
+		if(type==1) {
+			output = input*1.8+32;
+		}
+		else {
+			output = (input-32)/1.8;
+		}
+		System.out.println("The tempuratur converstion is");
+		System.out.print(output);
+	}
+
+}
