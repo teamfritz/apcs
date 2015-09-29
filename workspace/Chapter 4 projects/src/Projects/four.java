a0eafad7bb13b1e008d0a73f2d3a957bf32aa98a
teamfritz <cyberpatriotfritz@gmail.com>
2015-09-29 13:12:19 -0700

    9/29-1

--- workspace/Chapter 4 projects/src/Projects/four.java
+++ workspace/Chapter 4 projects/src/Projects/four.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 public class four {
 
 public static void main(String[] args) {
-		
+		long number = (long) prompt("an integer at least 0"), product = 1, count = 1;
+		while (count <= number) {
+			product = product*count;
+			ezprint((double)product);
+			count++;
+		}
+		ezprint("The factorial of " + number + " is " + product);
 	}
+
+/*****************************************************
+20! Shows up as negative because the integer goes so high that it runs out of memory and rolls over to the lowest possible value.
+Now that it is of type "long" the number can be larger before it rolls over.
+The new maximum value is: 20
+*****************************************************/
 	
 	public static void ezprint(String statement) {
 		System.out.println(statement);
