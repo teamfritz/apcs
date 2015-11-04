e1c3d4b23f52cf106bf611d77793e40b353375d2
teamfritz <cyberpatriotfritz@gmail.com>
2015-11-04 13:58:59 -0800

    11/4-1 :jack_o_lantern:

--- workspace/Chapter 7/src/projects/Seven.java
+++ workspace/Chapter 7/src/projects/Seven.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 
 public class Seven {
 	public static void main(String[] args) {
-		
+		int width = 0, height = 0;
+		for (height = 0; height <= 10; height++) {
+			for (width = 0; width<= 10; width++) {
+				if (height+width < 10) {
+					ezprint(" ", false);
+				}
+				if (height+width == 0) {
+					ezprint("  ", false);
+				} else {
+					ezprint(" " + ((int)(height+width)), false);
+				}
+			}
+			ezprint("");
+		}
 	}
 	
 	public static void ezprint(String statement) {
@@ -35,5 +48,17 @@ public class Seven {
 		double output = read.nextDouble();
 		return output; //returns a double value
 	}
-
+	
+	public static double prompt(String type, double min, double max) {    //allows you to prompt for a value with a pseudo-customized message
+		Scanner read = new Scanner(System.in);    //usage: <variable> = prompt(<type>)
+		double output;
+		while (true) {
+			System.out.println("Please input " + type);
+			output = read.nextDouble();
+			if (output >= min && output <= max)
+				break;
+			System.out.println("I'm sorry, that is not a valid input. It must be within the range [" + min + "-" + max + "]");
+		}
+		return output; //returns a double value
+	}
 }
